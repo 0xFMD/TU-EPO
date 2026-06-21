aa39dc9e23c470d3e7fcccb41ab0184283a9beca
0xFMD <30713087+0xFMD@users.noreply.github.com>
2026-06-22 01:05:25 +0300

    feat(projects/week9-02): VM with IO devices

---
+++ projects/week-9/02/README.md
@@ -0,0 +1,12 @@
+# Week 9 - VM - IO Devices
+
+## Compilation & Run
+
+```bash
+gcc -g ./zvm.c ./zvm_exception.c ./zvm_instruction.c ./zvm_io.c -o zvm
+
+./zvm
+```
+
+## Output
+The program will run a simple virtual machine that simulates IO devices. It will perform input and output operations, showing how the virtual machine handles IO requests.
---
+++ projects/week-9/02/zvm_io.c
@@ -0,0 +1,22 @@
+#include "zvm_io.h"
+
+bool zfnio_keyboard(zvm_vm_t *vm, uint8_t port, uint8_t operation,
+                    uint8_t argc) {
+
+  for (uint8_t i = 0; i < argc; i++) {
+    scanf(" %c", &vm->io_devices[port]->O[i]);
+  }
+
+  return true;
+}
+
+bool zfnio_screen(zvm_vm_t *vm, uint8_t port, uint8_t operation, uint8_t argc) {
+
+  /* functions:
+    0: write one character
+  */
+  // 1, 0, argc
+  uint8_t character = vm->io_devices[port]->I[0];
+  printf("%c", character);
+  return true;
+}
